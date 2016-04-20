f2a8819be3e50aea6ef3c5cabba98d8a64a0f47f
selbekk <kristofer.selbekk@bekk.no>
2016-04-20 20:19:49 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Contact form
+
+Let your users get in touch by adding a simple and customizable contact form to
+your Enonic XP website!
---
+++ src/main/resources/services/contact-form/contact-form.js
@@ -0,0 +1,14 @@
+exports.get = function(req) {
+    // TODO: Check origin to make sure it's from the same origin
+    // TODO: Verify input
+    // TODO: Send email
+    // TODO: Send OK response to consumer
+
+    return {
+        body: {
+            
+        },
+        contentType: 'application/json'
+    };
+
+};
